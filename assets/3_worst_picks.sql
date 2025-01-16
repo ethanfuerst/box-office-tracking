0c5468a38383b20137a481c48c6a94816fbf86f9
Ethan Fuerst <34084535+ethanfuerst@users.noreply.github.com>
2025-01-16 17:58:04 -0500

    linting

--- assets/3_worst_picks.sql
+++ assets/3_worst_picks.sql
@@ -11,20 +11,11 @@ create or replace table worst_picks as (
 
     , better_picks as (
         select
-            base.title
-            , base.multiplier
-            , count(distinct better_pick.title) as number_of_better_picks
-            , max(better_pick.scored_revenue) as max_better_pick_revenue
-        from base_query_cte as base
-        left join base_query_cte as better_pick
-            on
-                base.scored_revenue < better_pick.scored_revenue
-                and base.overall_pick < better_pick.overall_pick
-                and base.drafted_by != better_pick.drafted_by
-        where
-            better_pick.scored_revenue > 0
-        group by
-            1, 2
+            title
+            , count(distinct title) as number_of_better_picks
+            , max(missed_revenue) as max_better_pick_revenue
+        from better_pick_int
+        group by 1
     )
 
     , final as (
@@ -40,8 +31,7 @@ create or replace table worst_picks as (
             , base_query_cte.drafted_by
             , base_query_cte.overall_pick
             , better_picks.number_of_better_picks
-            , better_picks.max_better_pick_revenue
-            - base_query_cte.scored_revenue as missed_revenue
+            , better_picks.max_better_pick_revenue as missed_revenue
         from base_query_cte
         inner join better_picks
             on base_query_cte.title = better_picks.title
@@ -59,4 +49,4 @@ create or replace table worst_picks as (
         , missed_revenue
     from final
     order by 1 asc
-)
+);
