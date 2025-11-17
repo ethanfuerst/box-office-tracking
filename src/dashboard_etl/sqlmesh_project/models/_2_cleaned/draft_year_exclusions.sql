b7af8d8482960f9e398b77f3dd5d37561ca6bd48
Ethan Fuerst <34084535+ethanfuerst@users.noreply.github.com>
2025-11-16 20:50:45 -0500

    removing unused code

--- src/dashboard_etl/sqlmesh_project/models/_2_cleaned/draft_year_exclusions.sql
+++
@@ -1,9 +0,0 @@
-MODEL (
-  name cleaned.draft_year_exclusions,
-  kind FULL
-);
-
-select
-    try_cast(value as varchar) as movie
-from raw.multipliers_and_exclusions
-where try_cast(type as varchar) = 'exclusion'
--- src/utils/read_config.py
+++ src/utils/read_config.py
@@ -1,41 +1,10 @@
-import time
-from typing import Dict, List
+from typing import Dict
 
 import yaml
 
 
-def get_top_level_config() -> Dict:
-    with open('src/config/config.yml', 'r') as yaml_in:
+def get_config_dict(config_path: str) -> Dict:
+    with open(config_path, 'r') as yaml_in:
         yaml_object = yaml.safe_load(yaml_in)
 
     return yaml_object
-
-
-def get_all_ids_from_config() -> List[str]:
-    config = get_top_level_config()
-
-    return list(config['dashboards'].keys())
-
-
-def get_config_for_id(id: str) -> Dict:
-    top_level_config = get_top_level_config()
-
-    config = top_level_config['dashboards'][id]
-    config['dashboard_id'] = id
-
-    timestamp = int(time.time())
-    config['database_file'] = f'box_office_db_{id}_{timestamp}.duckdb'
-
-    bucket_config = top_level_config['bucket']
-    config['bucket'] = bucket_config['bucket']
-
-    var_names = [
-        's3_read_access_key_id_var_name',
-        's3_read_secret_access_key_var_name',
-        's3_write_access_key_id_var_name',
-        's3_write_secret_access_key_var_name',
-    ]
-    for var_name in var_names:
-        config[var_name] = bucket_config.get(var_name, None)
-
-    return config
